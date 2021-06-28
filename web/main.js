0012b3ab15a5d4aff91ac114f2becdd105976420
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-06-29 01:56:41 +0900

    fix bugs

--- web/main.js
+++ web/main.js
@@ -1,17 +1,15 @@
-import init, { han2zen } from "../pkg/han2zen.js";
-init().then(() => {
-	document.getElementById("convert").addEventListener("click", () => {
-		let input = document.getElementById("input");
-		performance.mark("start");
-		let result = han2zen(input.value);
-		performance.mark("end");
-		document.getElementById("output").innerText = result;
-		performance.measure(
-			"measure",
-			"start",
-			"end"
-		);
-		const performanceResult = performance.getEntriesByName("measure");
-		alert(performanceResult[0].duration + "ms");
-	});
+import { han2zen } from "../pkg/han2zen";
+document.getElementById("convert").addEventListener("click", () => {
+	let input = document.getElementById("input");
+	performance.mark("start");
+	let result = han2zen(input.value);
+	performance.mark("end");
+	document.getElementById("output").innerText = result;
+	performance.measure(
+		"measure",
+		"start",
+		"end"
+	);
+	const performanceResult = performance.getEntriesByName("measure");
+	alert(performanceResult[0].duration + "ms");
 });
